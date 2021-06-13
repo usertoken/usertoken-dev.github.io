361d47dfa32dc96a720eb7e2b10c7c320db3f00a
Alex Davis <alex2006hw@gmail.com>
2021-06-13 00:24:45 +0000

    update-server-for-multicast-dns

---
+++ server/index.js
@@ -0,0 +1,48 @@
+'use strict';
+//
+let oracles =[]
+let sortedOracles = []
+//
+const { network, utils, resolvers }  = require('./lib/index');
+const { flattenFilterAndSort, copyfs } = utils;
+const { mdns } = resolvers;
+
+const startNetwork = async options => {
+  try {
+    return await network(options)
+  } catch(e){
+    console.log('1.app network error:',e)
+  }
+}
+const app = async options => {
+  // try {
+    // const { gun, root } = 
+    const { root, gun } = await startNetwork(options)
+    if (root && root.get) {
+      // const root = gun.get('root')
+      root.get('address').once((address, index)=> {
+        // console.log('1.app address:', address)
+      })
+      root.get('port').once((port, index)=> {
+        // console.log('1.app port:', port)
+      })
+      root.get('peers').once((peers, value)=> {
+        // console.log('1.app peers:', peers)
+        mdns.start({root,peers});
+      })
+      root.get('oracles').map().once((oracle, value)=> {
+        oracles.push(value)
+        sortedOracles = flattenFilterAndSort(oracles)
+        // console.log('2.app oracles:', oracles)
+        // console.log('3.app sortedOracles:',sortedOracles)
+      })
+      // return ({root,gun})
+    }
+  // } catch(e) {
+    // console.log('1.app error:',e)
+    return ({root,gun})
+  // }
+}
+app({})
+//
+module.exports = app;
